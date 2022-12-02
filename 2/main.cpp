505f4cc3f669017b848266016a5cec86492d3928
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-02 06:32:36 +0100

    Fri Dec  2 06:32:36 AM CET 2022

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,4 +1,4 @@
-// #define TEST
+#define TEST
 
 #ifdef TEST
     #include "in_test.hpp"
@@ -9,15 +9,15 @@
 
 int check(std::string A)
 {
-    if(A == "A X")return 3+1;
-    if(A == "A Y")return 6+2;
-    if(A == "A Z")return 0+3;
+    if(A == "A X")return 0+3;
+    if(A == "A Y")return 3+1;
+    if(A == "A Z")return 6+2;
     if(A == "B X")return 0+1;
     if(A == "B Y")return 3+2;
     if(A == "B Z")return 6+3;
-    if(A == "C X")return 6+1;
-    if(A == "C Y")return 0+2;
-    if(A == "C Z")return 3+3;
+    if(A == "C X")return 0+2;
+    if(A == "C Y")return 3+3;
+    if(A == "C Z")return 6+1;
 
     P("error");
     exit(1);
