37b3a333730b26efd69ca0a925faafcf0b1ec207
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-04 06:11:24 +0100

    Sun Dec  4 06:11:23 AM CET 2022

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,4 +1,4 @@
-// #define TEST
+#define TEST
 
 #ifdef TEST
     #include "in_test.hpp"
@@ -17,6 +17,23 @@ int main(int argc, char** argv)
     auto in = getInput();
     ULL score = 0;
 
+    for(auto ss:in)
+    {
+        auto v0 = splitStr(ss[0],'-');
+        auto v1 = splitStr(ss[1],'-');
+
+        if(stoi(v1[0]) <= stoi(v0[0]) && stoi(v1[1]) >= stoi(v0[1]))
+        {
+            P_VECH(ss);
+            score++;
+        }
+        else if(stoi(v0[0]) <= stoi(v1[0]) && stoi(v0[1]) >= stoi(v1[1]))
+        {
+            P_VECH(ss);
+            score++;
+        }
+
+    }
 
     P(score);
 }
