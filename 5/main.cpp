2bb55c686b65b0b075ab7ffda00ca1d62e60c1b3
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-05 06:28:45 +0100

    Mon Dec  5 06:28:45 AM CET 2022

--- 5/main.cpp
+++ 5/main.cpp
@@ -18,9 +18,31 @@ int main(int argc, char** argv)
     ULL score = 0;
 
     std::vector<std::vector<char>> b;
+#ifdef TEST
     b.push_back({'Z', 'N'});
     b.push_back({'M', 'C', 'D'});
     b.push_back({'P'});
+#else
+//         [M]     [B]             [N]
+// [T]     [H]     [V] [Q]         [H]
+// [Q]     [N]     [H] [W] [T]     [Q]
+// [V]     [P] [F] [Q] [P] [C]     [R]
+// [C]     [D] [T] [N] [N] [L] [S] [J]
+// [D] [V] [W] [R] [M] [G] [R] [N] [D]
+// [S] [F] [Q] [Q] [F] [F] [F] [Z] [S]
+// [N] [M] [F] [D] [R] [C] [W] [T] [M]
+//  1   2   3   4   5   6   7   8   9
+
+    b.push_back({"N","S","D","C","V","Q","T"});
+    b.push_back({"M","F","V"});
+    b.push_back({"F","Q","W","D","P","N","H","M"});
+    b.push_back({"D","Q","R","T","F"});
+    b.push_back({"R","F","M","N","Q","H","V","B"});
+    b.push_back({"C","F","G","N","P","W","Q"});
+    b.push_back({"W","F","R","L","C","T"});
+    b.push_back({"T","Z","N","S"});
+    b.push_back({"M","S","D","J","R","Q","H","N"});
+#endif
 
 
     for(auto s:in)
