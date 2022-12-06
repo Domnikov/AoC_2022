a1b6a678220ce56aceb56f3dad2a74865ba0caa5
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-06 06:10:34 +0100

    Tue Dec  6 06:10:34 AM CET 2022

--- 6/main.cpp
+++ 6/main.cpp
@@ -18,5 +18,26 @@ int main(int argc, char** argv)
     auto in = getInput();
     ULL score = 0;
 
+
+    std::map<char,ULL> map;
+    S s = in[0];
+
+    FOR(i,s.size())
+    {
+        if(map.size() < 3)
+        {
+            map[s[i]] = 0;
+        }
+        else{
+            if(map.count(s[i]) && map[s[i]] == 0)
+            {
+                map[s[i]] = i;
+                P(s[i]);
+                P(i);
+            }
+        }
+
+    }
+
     P(score);
 }
