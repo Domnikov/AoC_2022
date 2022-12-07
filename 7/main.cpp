6f51a8c00cf6c4b50e57c2abcd149616ce6b3299
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-07 06:21:40 +0100

    Wed Dec  7 06:21:40 AM CET 2022

--- 7/main.cpp
+++ 7/main.cpp
@@ -7,6 +7,15 @@
     #include "in.hpp"
 #endif
 
+S get_path(VECS pwd)
+{
+    S res;
+    for(auto d:pwd)
+    {
+        res += d;
+    }
+    return res;
+}
 
 int check(char c)
 {
@@ -16,6 +25,46 @@ int check(char c)
 int main(int argc, char** argv)
 {
     auto in = getInput();
+
+    std::map<S, ULL> fs;
+    VECS pwd;
+
+    FOR(i,in.size())
+    {
+        S s = in[i];
+        if(s == "$ cd ..")
+        {
+            pwd.erase(pwd.end()-1);
+        }
+        else if(s.find("$ cd /") == 0)
+        {
+            S dir = s.substr(3);
+            pwd.clear();
+            pwd.push_back(dir);
+        }
+        else if(s.find("$ cd ") == 0)
+        {
+            S dir = s.substr(3);
+            pwd.push_back("/" + dir);
+        }
+        else if (s == "$ ls")
+        {
+            ULL size{};
+            while(++i < in.size() && in[i][0] != '$')
+            {
+                size += stoi(splitStr(in[i],' ')[0]);
+            }
+            fs[get_path(pwd)] = size;
+            --i;
+        }
+    }
+    for(auto p : fs)
+    {
+        S dir = p.first;
+        ULL size = p.second;
+        P(dir,size);
+    }
+
     ULL score = 0;
     P(score);
 }
