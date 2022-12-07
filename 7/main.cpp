dce436a17a54b53c50cbc4f4a6f173e568072ef5
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-07 06:23:44 +0100

    Wed Dec  7 06:23:44 AM CET 2022

--- 7/main.cpp
+++ 7/main.cpp
@@ -29,27 +29,32 @@ int main(int argc, char** argv)
     std::map<S, ULL> fs;
     VECS pwd;
 
-    P(__LINE__);
     FOR(i,in.size())
     {
+    P(__LINE__);
         S s = in[i];
+        P(s);
         if(s == "$ cd ..")
         {
+    P(__LINE__);
             pwd.erase(pwd.end()-1);
         }
         else if(s.find("$ cd /") == 0)
         {
+    P(__LINE__);
             S dir = s.substr(3);
             pwd.clear();
             pwd.push_back(dir);
         }
         else if(s.find("$ cd ") == 0)
         {
+    P(__LINE__);
             S dir = s.substr(3);
             pwd.push_back("/" + dir);
         }
         else if (s == "$ ls")
         {
+    P(__LINE__);
             ULL size{};
             while(++i < in.size() && in[i][0] != '$')
             {
