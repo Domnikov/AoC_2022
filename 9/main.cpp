42ad977f17cc9260f849b34f66d07b8d3c4006b2
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-09 06:29:29 +0100

    Fri Dec  9 06:29:29 AM CET 2022

--- 9/main.cpp
+++ 9/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char** argv)
                 P_RR("T[%d,%d] \t",t.first,t.second);
             }
             P_RR("\n");
-            v[h]++;
+            v[t]++;
         }
 
     }
