7d10b30ac01cd25c399a84b532cd3e5775e28344
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-09 06:17:32 +0100

    Fri Dec  9 06:17:32 AM CET 2022

--- 9/in_test.hpp
+++ 9/in_test.hpp
@@ -4,6 +4,14 @@
 inline auto getInput()
 {
     VECS input {
+"R 4",
+"U 4",
+"L 3",
+"D 1",
+"R 4",
+"D 1",
+"L 5",
+"R 2",
     };
 
     return input;
--- 9/main.cpp
+++ 9/main.cpp
@@ -9,11 +9,63 @@
 
 auto in = getInput();
 
+struct point
+{
+    int x;
+    int y;
+
+    point& operator+(point s){x+=s.x;y+=s.y;return *this;}
+
+    void move(char c)
+    {
+        switch(c)
+        {
+            case 'U': y+=1;break;
+            case 'D': y-=1;break;
+            case 'L': x+=1;break;
+            case 'R': x-=1;break;
+        };
+    }
+};
+
 int main(int argc, char** argv)
 {
-    ULL score = 0;
+    int score = 0;
 
+    point h{0,0};
+    point t{0,0};
 
+    std::map<point, int> v;
+
+    for(auto p:in)
+    {
+        char cmd = p[0];
+        int val = stoi(p.substr(2));
+        FOR(i,val)
+        {
+            h.move(cmd);
+            if( abs(h.x-t.x) > 1 || abs(h.y-t.y) > 1 )
+            {
+                if(h.x == t.x)
+                {
+                    t.y += (h.y > t.y) ? 1 : -1;
+                }
+                else if(h.y == t.y)
+                {
+                    t.x += (h.x > t.x) ? 1 : -1;
+                }
+                else
+                {
+                    t.y += (h.y > t.y) ? 1 : -1;
+                    t.x += (h.x > t.x) ? 1 : -1;
+                }
+
+                v[t]++;
+            }
+        }
+
+    }
+    score = v.size();
     P(score);
 }
 
