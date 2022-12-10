f3d04dddb9e57dbf54c7e2a9d4b8b4d92d7b249b
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-10 06:20:46 +0100

    Sat Dec 10 06:20:46 AM CET 2022

--- 10/in_test.hpp
+++ 10/in_test.hpp
@@ -4,6 +4,9 @@
 inline auto getInput()
 {
     VECS input {
+"noop",
+"addx 3",
+"addx -5",
     };
 
     return input;
--- 10/main.cpp
+++ 10/main.cpp
@@ -9,9 +9,58 @@
 
 auto in = getInput();
 
+bool isSpecial(LL cycle)
+{
+    if(
+            cycle == 20 ||
+            cycle == 60 ||
+            cycle ==100 ||
+            cycle ==140 ||
+            cycle ==180 ||
+            cycle ==220) return true;
+
+    return false;
+}
+
+int check(S cmd, int val)
+{
+    static int cycle = 0;
+    static int sum = 1;
+    int newScore = 0;
+    if(cmd == "noop")
+    {
+        if(isSpecial(++cycle))
+        {
+            newScore = sum;
+        }
+    }
+    else if(cmd == "addx")
+    {
+        if(isSpecial(++cycle))
+        {
+            newScore = sum;
+        }
+        if(isSpecial(++cycle))
+        {
+             sum += val;
+             newScore += sum;
+        }
+    }
+    return newScore;
+}
+
 int main(int argc, char** argv)
 {
     int score = 0;
+
+
+    for(auto s:in)
+    {
+        auto vec = splitStr(s,' ');
+
+        int addScore{}, addCycle{};
+        score += check(vec[0], vec[1].empty() ? 0 : stoi(vec[1]));
+    }
 
     P(score);
 }
