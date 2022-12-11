9da6d70fa293fad5b0c95a7909d852f147c1d57f
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-11 06:33:46 +0100

    Sun Dec 11 06:33:46 AM CET 2022

--- 11/main.cpp
+++ 11/main.cpp
@@ -55,11 +55,11 @@ void check(M& m)
 {
     FOR(i, m.items.size())
     {
-        auto& it = m.items.front();
-        m.items.pop();
-        it = m.worryCalc(it)/3;
-        int to = m.test(it) ? m.ifTrue : m.ifFalse;
-        mm[to].items.push(to);
+        // auto& it = m.items.front();
+        // m.items.pop();
+        // it = m.worryCalc(it)/3;
+        // int to = m.test(it) ? m.ifTrue : m.ifFalse;
+        // mm[to].items.push(to);
     }
 }
 
