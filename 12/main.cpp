5cf42df6fb1bd3dd3929122afff0ecbb97771a0b
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-12 06:42:32 +0100

    Mon Dec 12 06:42:32 AM CET 2022

--- 12/main.cpp
+++ 12/main.cpp
@@ -18,7 +18,7 @@ std::set<std::pair<int,int>> inv(int X, int Y, char cur)
     for(int y = std::max<int>(0, Y-1); y <= std::min<int>(Y+1, in.size());y++)
         for(int x = std::max<int>(0, X-1); x <= std::min<int>(X+1, in[y].size());x++)
         {
-            P(((abs(X-x)+abs(Y-y)) ==1));
+            P(X, x, Y, x,((abs(X-x)+abs(Y-y)) ==1));
             if(x != X && y != Y && ((abs(X-x)+abs(Y-y)) ==1))
             {
             int dif = in[y][x]-cur;
