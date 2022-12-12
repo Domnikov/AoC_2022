a3890c0731bb4f23302df82d86706780359f8a4f
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-12 06:52:01 +0100

    Mon Dec 12 06:52:01 AM CET 2022

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,4 +1,4 @@
-#define TEST
+// #define TEST
 #include <stack>
 
 #ifdef TEST
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
     }
 
 
-    FOR(n,40)
+    FOR(n,4000)
     {
         P(n);
         score++;
