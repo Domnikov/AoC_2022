009830817439e1f8564cf47ae7ab70fdfccda59a
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-13 12:14:29 +0100

    add template 14

---
+++ 14/main.cpp
@@ -0,0 +1,26 @@
+#define TEST
+
+#ifdef TEST
+    #include "in_test.hpp"
+#else
+    #include "in.hpp"
+#endif
+
+auto in = getInput();
+bool D = true;
+using INT = __int128;
+
+int main(int argc, char** argv)
+{
+    LL score = 0;
+    D = false;
+    for(int i{}; i < in.size();i+=3)
+    {
+    }
+    P_RR("Part1: %lld\n", score);
+
+    D = false;
+    score = 0;
+
+    P_RR("Part2: %lld\n", score);
+}
