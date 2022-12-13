7b175334b13487d8fede641f333af97a335d26a1
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-13 08:02:38 +0100

    Tue Dec 13 08:02:38 AM CET 2022

--- 13/main.cpp
+++ 13/main.cpp
@@ -1,4 +1,4 @@
-// #define TEST
+#define TEST
 
 #ifdef TEST
     #include "in_test.hpp"
@@ -26,113 +26,180 @@ bool isDigit(S s)
     return true;
 }
 
-S getNext(S& s)
+bool isList(S& s)
 {
-    if(s.empty()) return {};
-    if(s[0] == ',')
-    {
-        s = s.substr(1);
-    }
-    if(s[0] == '[')
-    {
-        s = s.substr(1);
-        return "[";
-    }
-    if(s[0] == ']')
-    {
-        s = s.substr(1);
-        return {};
-    }
-    if(isDigit(s[0]))
-    {
-        int len{};
-        while (isDigit(s[++len])){}
-        S res = s.substr(0, len);
-        s = s.substr(len);
-        return res;
-    }
-    P_RR("Oops. Nothing to do!!!\n");
-    exit(1);
+    return !isDigit(s);
 }
 
-VECI getList(S& s)
+// S getNext(S& s)
+// {
+//     if(s.empty()) return {};
+//     if(s[0] == ',')
+//     {
+//         s = s.substr(1);
+//     }
+//     if(s[0] == '[')
+//     {
+//         s = s.substr(1);
+//         return "[";
+//     }
+//     if(s[0] == ']')
+//     {
+//         s = s.substr(1);
+//         return {};
+//     }
+//     if(isDigit(s[0]))
+//     {
+//         int len{};
+//         while (isDigit(s[++len])){}
+//         S res = s.substr(0, len);
+//         s = s.substr(len);
+//         return res;
+//     }
+//     P_RR("Oops. Nothing to do!!!\n");
+//     exit(1);
+// }
+//
+// VECI getList(S& s)
+// {
+//     VECS res;
+//     if(s.empty())return res;
+//     bool isList = (s[0] == '[');
+//     P(isList, s[0]);
+//     S next = getNext(s);
+//     while(!next.empty())
+//     {
+//         if(isDigit(next)) res.push_back(stoi(next));
+//         if(!res.empty() && next == "[")
+//         {
+//             break;
+//         }
+//         if(!isList)
+//         {
+//             break;
+//         }
+//         next = getNext(s);
+//     }
+//     return res;
+// }
+
+
+VECS getList(S s)
 {
-    VECI res;
-    if(s.empty())return res;
-    bool isList = (s[0] == '[');
-    P(isList, s[0]);
-    S next = getNext(s);
-    while(!next.empty())
+    VECS res;
+    int bCnt = 0;
+    size_t elSt{};
+    FOR(i, s.size())
     {
-        if(isDigit(next)) res.push_back(stoi(next));
-        if(!res.empty() && next == "[")
+        char& c = s[i];
+        if(c == '[')
         {
-            break;
+            bCnt++;
         }
-        if(!isList)
+        if(c == ']')
         {
-            break;
+            bCnt--;
         }
-        next = getNext(s);
+        if(bCnt == 0)
+        {
+            if(c == ',')
+            {
+                res.push_back(s.substr(elSt, i-elSt));
+                elSt = i;
+            }
+        }
+    }
+    if(elSt != s.size()-1)
+    {
+        res.push_back(s.substr(elSt));
+    }
+    if(res.size() == 1 && !isDigit(res[0]))
+    {
+        res = getList(res[0]);
     }
     return res;
+}
+
+
+int compareLists(VECS& l1, VECS& l2)
+{
+    FOR(i, +std::max(l1.size(), l2.size()))
+    {
+        if(i == l1.size() || i == l2.size())
+        {
+            if(l1.size() > l2.size())
+            {
+                P_RR("right out of elements\n");
+                return 1;
+            }
+            if(l1.size() < l2.size())
+            {
+                P_RR("left out of elements\n");
+                return -1;
+            }
+            else
+            {
+                P_RR("Both out of elements");
+                break;
+            }
+        }
+        else
+        {
+            if(!isList(l1[i]) && !isList(l2[i]))
+            {
+                LL v1 = stoi(l1[i]);
+                LL v2 = stoi(l2[i]);
+                if(v1 < v2)
+                {
+                    P_RR("Left lower: %lld < %lld\n", v1, v2);
+                    return -1;
+                }
+                else if(v1 > v2)
+                {
+                    P_RR("Right lover: %lld > %lld\n", v1, v2);
+                    return 1;
+                }
+            }
+            else
+            {
+                VECS list1 = getList(l1[i]);
+                VECS list2 = getList(l2[i]);
+                auto res = compareLists(list1, list2);
+                if(res != 0){return res;}
+            }
+        }
+    }
+    if(l2.empty())
+    {
+        P_RR("right out of elements\n");
+        return 1;
+    }
+    if(l1.empty())
+    {
+        P_RR("left out of elements\n");
+        return -1;
+    }
+    P_RR("Equals -> continue\n");
 }
 
 bool compare(S s1, S s2)
 {
     P(s1, s2);
-    auto b1 = s1.begin();
-    auto b2 = s2.begin();
-    for(int n = 0; true; ++n)
+    auto v1 = getList(s1);
+    auto v2 = getList(s2);
+    P_RR("Compare:\n");
+    P_VEC(v1);
+    P_VEC(v2);
+    if(compareLists(v1, v2) < 0)
     {
-        auto v1 = getList(s1);
-        auto v2 = getList(s2);
-        P_RR("Compare:\n");
-        P_VEC(v1);
-        P_VEC(v2);
-        FOR(i, +std::max(v1.size(), v2.size()))
-        {
-            if(i == v1.size() || i == v2.size())
-            {
-                if(v1.size() > v2.size())
-                {
-                    P_RR("right out of elements\n");
-                    return false;
-                }
-                if(v1.size() < v2.size())
-                {
-                    P_RR("left out of elements\n");
-                    return true;
-                }
-                else
-                {
-                    P_RR("Both out of elements");
-                    break;
-                }
-            }
-            else if(v1[i] < v2[i])
-            {
-                P_RR("Left lower: %lld < %lld\n", v1[i], v2[i]);
-                return true;
-            }
-            else if(v1[i] > v2[i])
-            {
-                P_RR("Right lover: %lld > %lld\n", v1[i], v2[i]);
-                return false;
-            }
-        }
-        if(s2.empty())
-        {
-            P_RR("right out of elements\n");
-            return false;
-        }
-        if(s1.empty())
-        {
-            P_RR("left out of elements\n");
-            return true;
-        }
-        P_RR("Equals -> continue\n");
+        return true;
     }
+    if(compareLists(v1, v2) > 0)
+    {
+        return false;
+    }
+    P_RR("Equal\n");
+    exit(1);
 }
 
 int main(int argc, char** argv)
@@ -140,8 +207,8 @@ int main(int argc, char** argv)
     LL score = 0;
     for(int i{}; i < in.size();i+=3)
     {
-        auto s1 = in[i].substr(1, in[i].size()-2);
-        auto s2 = in[i+1].substr(1, in[i+1].size()-2);
+        auto s1 = in[i];
+        auto s2 = in[i+1];
         if(in[i+2].size() > 0){P("MUST BE EMPTY");exit(1);}
         auto res = compare(s1, s2);
         if(res)
