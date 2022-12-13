8d5a0b6d557ce8ad5eb70e861b591bebd84eb3a5
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-13 12:03:55 +0100

    Tue Dec 13 12:03:55 PM CET 2022

--- 13/main.cpp
+++ 13/main.cpp
@@ -183,7 +183,7 @@ int main(int argc, char** argv)
     in.push_back("[[6]]");
     // in.shrink_to_fit();
 
-    std::sort(BE(in), [](const S& a, const S&b){return compare(a, b) ? false : true;});
+    std::sort(BE(in), [](const S& a, const S&b){return !compare(a, b) ? false : true;});
 
 
     P_VECV(in);
