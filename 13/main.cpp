28872673d02da394919b0ce45a518f8d3b6771c8
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-13 11:55:49 +0100

    Tue Dec 13 11:55:49 AM CET 2022

--- 13/main.cpp
+++ 13/main.cpp
@@ -140,7 +140,7 @@ int compare(const S& s1, const S& s2)
     P_VEC(v1);
     auto v2 = getList(s2);
     P_VEC(v2);
-    auto res = compareLists(v1, v2);
+    auto res = v1<v2 ? -1 : 1;//compareLists(v1, v2);
     P(res);
     if(res < 0)
     {
