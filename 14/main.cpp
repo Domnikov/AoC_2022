f489feeb6a2fdcf2d7b54d4e69383615fe340c49
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-14 06:05:35 +0100

    Wed Dec 14 06:05:35 AM CET 2022

--- 14/main.cpp
+++ 14/main.cpp
@@ -10,13 +10,25 @@ auto in = getInput();
 bool D = true;
 using INT = __int128;
 
+VECS grid(1000, S(1000, ' '));
+
 int main(int argc, char** argv)
 {
     LL score = 0;
     D = false;
-    for(int i{}; i < in.size();i+=3)
+    for(int i{}; i < in.size();i++ )
     {
+        S line = in[i];
+        while(!line.empty())
+        {
+            auto pos = line.find(" -> ");
+            S cur = line.substr(0, pos);
+            line = line.substr(pos+4);
+            P(cur,line);
+        }
+        P("");
     }
+    P_VECV(grid);
     P_RR("Part1: %lld\n", score);
 
     D = false;
