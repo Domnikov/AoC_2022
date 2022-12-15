6558fa52beed86efadd103c107f1888c04b613b1
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-15 06:39:21 +0100

    Thu Dec 15 06:39:21 AM CET 2022

--- 15/main.cpp
+++ 15/main.cpp
@@ -1,8 +1,10 @@
 #define TEST
 
 #ifdef TEST
+    LL L = 20;
     #include "in_test.hpp"
 #else
+    LL L = 2000000;
     #include "in.hpp"
 #endif
 
@@ -10,40 +12,50 @@ auto in = getInput();
 bool D = true;
 using INT = __int128;
 
-std::vector<std::pair<int, int>> sen;
-std::vector<std::pair<int, int>> bec;
+std::vector<std::pair<LL, LL>> sen;
+std::vector<std::pair<LL, LL>> bec;
+VECI senPwr;
 VECS grid(30, S(30, ' '));
 
-int X(int x){return x+2;}
-int Y(int y){return y+2;}
+LL X(LL x){return x+2;}
+LL Y(LL y){return y+2;}
 
 int main(int argc, char** argv)
 {
     LL score = 0;
     D = false;
+    LL minx = 999999999, maxx = 0;
     for(int i{}; i < in.size();i++ )
     {
         S line = in[i];
-        P(line);
         auto vec = splitStr(line, ' ');
-        int sx = stoi(vec[2].substr(2, vec[2].size() - 3));
-        P(sx);
-        int sy = stoi(vec[3].substr(2, vec[3].size() - 3));
-        P(sy);
-        int bx = stoi(vec[8].substr(2, vec[8].size() - 3));
-        P(bx);
-        int by = stoi(vec[9].substr(2                   ));
-        P(by);
+        LL sx = stoi(vec[2].substr(2, vec[2].size() - 3));
+        LL sy = stoi(vec[3].substr(2, vec[3].size() - 3));
+        LL bx = stoi(vec[8].substr(2, vec[8].size() - 3));
+        LL by = stoi(vec[9].substr(2                   ));
 
         sen.push_back({sx, sy});
         bec.push_back({bx, by});
+        senPwr.push_back(abs(sx-bx) + abs(sy-by));
 
-        grid[Y(sy)][X(sx)] = 'S';
-        grid[Y(by)][X(bx)] = 'B';
+        // grid[Y(sy)][X(sx)] = 'S';
+        // grid[Y(by)][X(bx)] = 'B';
 
-    P_VECV(grid);
+        minx = std::min(minx, sx-senPwr.back());
+        maxx = std::max(maxx, sx+senPwr.back());
+
     }
-    P_VECV(grid);
+    // P_VECV(grid);
+    for(LL i = minx; i <= maxx;++i)
+    {
+        FOR(id, sen.size())
+        {
+            LL sx = sen[id].first;
+            LL sy = sen[id].second;
+            LL dist = abs(sx-i) + abs(sy-L);
+            if(dist <= senPwr[id]) score++;
+        }
+    }
 
     P_RR("Part1: %lld\n", score);
 
