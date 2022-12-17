717035fc114266d50f14a2a13ea7a8bfbc3b7b0c
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-17 16:47:28 +0100

    Sat Dec 17 04:47:28 PM CET 2022

--- 16/main.cpp
+++ 16/main.cpp
@@ -111,6 +111,7 @@ VECI calc(VECI path, LL time)
             newV.push_back(V.back()[0]);
             for(LL j = 1; j < N;++j)
             {
+                P(j);
                 LL maxScore = 0;
                 VECI maxVector;
                 for(LL i = 1; i < V.size(); ++i)
