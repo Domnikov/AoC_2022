088a0d51a21b20b7a96219df0ef348c81c6b9eed
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-18 17:14:49 +0100

    Sun Dec 18 05:14:49 PM CET 2022

--- 18/main.cpp
+++ 18/main.cpp
@@ -74,7 +74,7 @@ std::set<std::tuple<LL,LL,LL>> inside(std::tuple<LL,LL,LL> p)
     if(isInside)
     {
         auto [x, y, z] = p;
-        // P(x, y, z);
+        P(x, y, z);
         P_VEC(vec);
         // for(auto v : vis) {auto [xx, yy, zz] = v; P(xx, yy, zz);P_VEC(surf[v]);}
     }
