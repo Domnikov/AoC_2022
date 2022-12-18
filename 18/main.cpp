ad36d960af61f99f43a24baeb1e22ea4f0de2dd8
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-18 17:26:21 +0100

    Sun Dec 18 05:26:21 PM CET 2022

--- 18/main.cpp
+++ 18/main.cpp
@@ -46,10 +46,10 @@ VECI sides(std::tuple<LL,LL,LL> p, std::set<std::tuple<LL,LL,LL>>& vis)
     {
         vis.insert(p);
         {
-            // auto [x, y, z] = p;
-            // P(x, y, z);
+            auto [x, y, z] = p;
+            P(x, y, z);
             vec = surf[p];
-            // P_VEC(vec);
+            P_VEC(vec);
             add(vec, sides(u(p), vis));
             add(vec, sides(l(p), vis));
             add(vec, sides(r(p), vis));
