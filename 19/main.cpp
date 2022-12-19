d03e4f9fdfd396c4026051fc8fa8583c3994fc7a
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 08:25:51 +0100

    Mon Dec 19 08:25:51 AM CET 2022

--- 19/main.cpp
+++ 19/main.cpp
@@ -13,33 +13,39 @@ using VECC = std::vector<char>;
 LL T = 24;
 
 
-LL calc(VECI cost, VECI rob, VECI res, LL time)
+LL calc(VECII cost, VECI strtg)
 {
-    if(time >= 24){if(res[3] > 9){P(res[3]);} return res[3];}
-    LL oreCost     = cost[0];
-    LL clayCost    = cost[1];
-    LL obsOreCost  = cost[2];
-    LL obsClayCost = cost[3];
-    LL geoOreCost  = cost[4];
-    LL goeObsCost  = cost[5];
-    LL ore = res[0], clay = res[1], obs = res[2], geo = res[3];
-    LL orR = rob[0], clR = rob[1], obR = rob[2], geR = rob[3];
+    LL res[4];
+    LL rob[4];
 
 
-    LL score{};
+    LL next{};
+    FOR(t, T)
+    {
 
-    ore += orR;
-    clay += clR;
-    obs += obR;
-    geo += geR;
+        if(
+                res[0] >= cost[strtg[next]][0] &&
+                res[1] >= cost[strtg[next]][1] &&
+                res[2] >= cost[strtg[next]][2]
+                )
+        {
+            FOR(i, 4) {res[i] += rob[i];}
+            res[0] -= cost[strtg[next]][0];
+            res[1] -= cost[strtg[next]][1];
+            res[2] -= cost[strtg[next]][2];
+            rob[next++]++;
+        }
+        else
+        {
+            FOR(i, 4) {res[i] += rob[i];}
+        }
+        P(t);
+        P(res[0], res[1], res[2], res[3]);
+        P(rob[0], rob[1], rob[2], rob[3]);
+    };
 
-    if(res[0] >= oreCost                             && orR <= (clR+1) * 3                    ) { score = std::max(score, calc(cost, {orR+1, clR, obR, geR}, {ore - oreCost, clay, obs, geo}, time+1)); }
-    if(res[0] >= clayCost                            && clR <= (orR+1) * 3 && clR <= (obR+1)*2  ) { score = std::max(score, calc(cost, {orR, clR+1, obR, geR}, {ore - clayCost, clay, obs, geo}, time+1)); }
-    if(res[0] >= obsOreCost && res[1] >= obsClayCost && obR <= (geR+1) * 3                    ) { score = std::max(score, calc(cost, {orR, clR, obR+1, geR}, {ore - obsOreCost, clay - obsClayCost, obs, geo}, time+1)); }
-    if(res[0] >= geoOreCost && res[2] >= goeObsCost                                           ) { score = std::max(score, calc(cost, {orR, clR, obR, geR+1}, {ore - geoOreCost, clay, obs - goeObsCost, geo}, time+1)); }
-    if(ore <= std::max(std::max(std::max(oreCost, clayCost), obsOreCost), geoOreCost)         ) { score = std::max(score, calc(cost, {orR, clR, obR, geR}, {ore, clay, obs, geo}, time+1)); }
 
-    return score;
+    return res[3];
 }
 
 
@@ -57,7 +63,8 @@ int main(int argc, char** argv)
         LL obsClayCost = stoi(sent[21]);
         LL geoOreCost  = stoi(sent[27]);
         LL goeObsCost  = stoi(sent[30]);
-        score += ++num * calc({oreCost, clayCost, obsOreCost, obsClayCost, geoOreCost, goeObsCost}, {1, 0, 0, 0}, {0, 0, 0, 0}, 0);
+        VECII cost{{oreCost, 0, 0}, {clayCost, 0, 0}, {obsOreCost, obsClayCost, 0}, {geoOreCost, 0, goeObsCost}};
+        score += ++num * calc(cost, {1, 1, 1, 2, 1, 2, 3, 3});
     }
 
     P_RR("Part1: %lld\n", score);
