5a8b6d969dd5d8d9bb91fd7b79227097e1bc2246
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 15:24:47 +0100

    Mon Dec 19 03:24:47 PM CET 2022

--- 19/main.cpp
+++ 19/main.cpp
@@ -41,17 +41,19 @@ LL calc(char cr1o, char cr2o, char cr3o, char cr3c, char cr4o, char cr4b)
 
     LL next{};
     LL max{};
+    LL maxB{};
     char count{};
     while(!q.empty())
     {
         auto [rob1, rob2, rob3, rob4, res1, res2, res3, res4, time] = q.front();
         q.pop();
         max = std::max<LL>(max, res4);
+        maxB= std::max<LL>(maxB,res3);
         if(time == T) {if(res4 == 31)P((int)rob1, (int)rob2, (int)rob3, (int)rob4, (int)res4, max);continue;}
         if(time >= 19 && rob3 == 0) continue;
         if(time >= 23 && rob4 == 0) continue;
         if(rob1 > 9 || rob2 > 9 || rob3 > 9) continue;
-        if(count < time){P((int)time, q.size(), (int)rob1, (int)rob2, (int)rob3, (int)rob4, max);count = time;max = 0;}
+        if(count < time){P((int)time, q.size(), (int)rob1, (int)rob2, (int)rob3, (int)rob4, max, maxB);count = time;max = 0;maxB = 0;}
         unsigned char Nres1 = rob1+res1;
         unsigned char Nres2 = rob2+res2;
         unsigned char Nres3 = rob3+res3;
