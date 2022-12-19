5f1aefde95f1ecc624aea42a79483af570ac8773
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 20:44:13 +0100

    Mon Dec 19 08:44:13 PM CET 2022

--- 19/main.cpp
+++ 19/main.cpp
@@ -1,4 +1,4 @@
-// #define TEST
+#define TEST
 
 #ifdef TEST
 #include "in_test.hpp"
@@ -41,7 +41,7 @@ LL calc(unsigned char cr1o, unsigned char cr2o, unsigned char cr3o, unsigned cha
 
     LL next{};
     LL max{};
-    LL maxB{};
+    // LL maxB{};
     char count{};
     P((int)cr1o, (int)cr2o, (int)cr3o, (int)cr3c, (int)cr4o, (int)cr4b);
     LL maxR{};
@@ -49,29 +49,28 @@ LL calc(unsigned char cr1o, unsigned char cr2o, unsigned char cr3o, unsigned cha
     {
         auto [res1, res2, res3, res4, rob1, rob2, rob3, rob4, time] = q.front();
         q.pop();
-        max = std::max<LL>(max, res4);
-        maxB= std::max<LL>(maxB,res3);
+        // maxB= std::max<LL>(maxB,res3);
         LL Rsum = (rob1 + rob2 + 2*rob3 + 2*rob4);
         maxR = std::max<LL>(maxR, Rsum);
         if(time >= 13 && Rsum*9/5 < maxR)continue;
-        if(time == T) {continue;}
+        if(time == T) {max = std::max<LL>(max, res4);continue;}
         if(time >= 12 && rob2 == 0) continue;
         if(time >= 21 && rob3 == 0) continue;
         if(time >= 123 && rob4 == 0) continue;
-        if(rob1 > 9 || rob2 > 9 || rob3 > 9) continue;
-        if(count < time){P((int)time, q.size(), (int)rob1, (int)rob2, (int)rob3, (int)rob4, max, maxB);count = time;max = 0;maxB = 0;}
+        if(rob1 > 11 || rob2 > 11 || rob3 > 11) continue;
+        // if(count < time){P((int)time, q.size(), (int)rob1, (int)rob2, (int)rob3, (int)rob4, max, maxB);count = time;max = 0;maxB = 0;}
         unsigned char Nres1 = rob1+res1;
         unsigned char Nres2 = rob2+res2;
         unsigned char Nres3 = rob3+res3;
         unsigned char Nres4 = rob4+res4;
         unsigned char Mcmo  = std::max(cr1o , std::max(cr2o , std::max(cr3o , cr4o )));
-        if(time == 15 && res4 > 0){P((int)time, (int)rob1, (int)rob2, (int)rob3, (int)rob4, (int)res1, (int)res2, (int)res3, (int)res4, (int)Nres4);}
+        // if(time == 15 && res4 > 0){P((int)time, (int)rob1, (int)rob2, (int)rob3, (int)rob4, (int)res1, (int)res2, (int)res3, (int)res4, (int)Nres4);}
         // if(time>4)exit(1);
         if(res1 >= cr1o                ){q.push({(unsigned char)(Nres1-cr1o), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(rob1+1), (unsigned char)(rob2 ), (unsigned char)(rob3 ), (unsigned char)(rob4 ), (unsigned char)(time+1)});}
         if(res1 >= cr2o                ){q.push({(unsigned char)(Nres1-cr2o), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(rob1 ), (unsigned char)(rob2+1), (unsigned char)(rob3 ), (unsigned char)(rob4 ), (unsigned char)(time+1)});}
         if(res1 >= cr3o && res2 >= cr3c){q.push({(unsigned char)(Nres1-cr3o), (unsigned char)(Nres2-cr3c), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(rob1 ), (unsigned char)(rob2 ), (unsigned char)(rob3+1), (unsigned char)(rob4 ), (unsigned char)(time+1)});}
         if(res1 >= cr4o && res3 >= cr4b){q.push({(unsigned char)(Nres1-cr4o), (unsigned char)(Nres2     ), (unsigned char)(Nres3-cr4b), (unsigned char)(Nres4     ), (unsigned char)(rob1 ), (unsigned char)(rob2 ), (unsigned char)(rob3 ), (unsigned char)(rob4+1), (unsigned char)(time+1)});}
-        if(Mcmo*2 >= res1 && res3 <= cr4b*2){q.push({(unsigned char)(Nres1     ), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), rob1  , rob2  , rob3  , rob4  , (unsigned char)(time+1)});}
+        if(Mcmo*3 >= res1 && res3 <= cr4b*3){q.push({(unsigned char)(Nres1     ), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), rob1  , rob2  , rob3  , rob4  , (unsigned char)(time+1)});}
     }
 
 
@@ -95,9 +94,9 @@ int main(int argc, char** argv)
         unsigned char cr4b = stoi(sent[30]);
         LL max = std::max(max, calc(cr1, cr2, cr3o, cr3c, cr4o, cr4b));
 
-        P(num+1, max, score);
-
         score += ++num * max;
+        P(num, max, score);
+
     }
 
     P_RR("Part1: %lld\n", score);
