99465af351eb0ac677669194ef5016868c058109
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 23:52:58 +0100

    add template 20

---
+++ 20/main.cpp
@@ -0,0 +1,31 @@
+#define TEST
+
+#ifdef TEST
+#include "in_test.hpp"
+#else
+#include "in.hpp"
+#endif
+
+#include<queue>
+
+auto in = getInput();
+using INT = __int128;
+using VECC = std::vector<char>;
+
+
+int main(int argc, char** argv)
+{
+    LL score = 0;
+
+    for(auto& i : in)
+    {
+
+    }
+
+    P_RR("Part1: %lld\n", score);
+    //========================================================
+    score = 0;
+
+    P_RR("Part2: %lld\n", score);
+    return 0;
+}
