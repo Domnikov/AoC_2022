e0b3c257bab9cc78eb047b18a669815c5ed9b2a6
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 15:47:45 +0100

    Mon Dec 19 03:47:45 PM CET 2022

--- 19/main.cpp
+++ 19/main.cpp
@@ -62,11 +62,11 @@ LL calc(unsigned char cr1o, unsigned char cr2o, unsigned char cr3o, unsigned cha
         unsigned char Nres4 = rob4+res4;
         unsigned char Mcmo  = std::max(cr1o , std::max(cr2o , std::max(cr3o , cr4o )));
         P((int)time, (int)rob1, (int)rob2, (int)rob3, (int)rob4, (int)Nres1, (int)Nres2, (int)Nres3);
-        if(res1 >= cr1o                ){q.push({rob1++, rob2  , rob3  , rob4  , (unsigned char)(Nres1-cr1o), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(time+1)});}
-        if(res1 >= cr2o                ){q.push({rob1  , rob2++, rob3  , rob4  , (unsigned char)(Nres1-cr2o), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(time+1)});}
-        if(res1 >= cr3o && res2 >= cr3c){q.push({rob1  , rob2  , rob3++, rob4  , (unsigned char)(Nres1-cr3o), (unsigned char)(Nres2-cr3c), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(time+1)});}
-        if(res1 >= cr4o && res3 >= cr4b){q.push({rob1  , rob2  , rob3  , rob4++, (unsigned char)(Nres1-cr4o), (unsigned char)(Nres2     ), (unsigned char)(Nres3-cr4b), (unsigned char)(Nres4     ), (unsigned char)(time+1)});}
-        if(Mcmo >= res1 && res3 <= cr4b){q.push({rob1  , rob2  , rob3  , rob4  , (unsigned char)(Nres1     ), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), (unsigned char)(time+1)});}
+        if(res1 >= cr1o                ){q.push({(unsigned char)(Nres1-cr1o), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), rob1++, rob2  , rob3  , rob4  , (unsigned char)(time+1)});}
+        if(res1 >= cr2o                ){q.push({(unsigned char)(Nres1-cr2o), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), rob1  , rob2++, rob3  , rob4  , (unsigned char)(time+1)});}
+        if(res1 >= cr3o && res2 >= cr3c){q.push({(unsigned char)(Nres1-cr3o), (unsigned char)(Nres2-cr3c), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), rob1  , rob2  , rob3++, rob4  , (unsigned char)(time+1)});}
+        if(res1 >= cr4o && res3 >= cr4b){q.push({(unsigned char)(Nres1-cr4o), (unsigned char)(Nres2     ), (unsigned char)(Nres3-cr4b), (unsigned char)(Nres4     ), rob1  , rob2  , rob3  , rob4++, (unsigned char)(time+1)});}
+        if(Mcmo >= res1 && res3 <= cr4b){q.push({(unsigned char)(Nres1     ), (unsigned char)(Nres2     ), (unsigned char)(Nres3     ), (unsigned char)(Nres4     ), rob1  , rob2  , rob3  , rob4  , (unsigned char)(time+1)});}
     }
 
 
