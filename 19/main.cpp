34f99a7ebdba87c7870be4c6e098db35cbaf5afb
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 15:42:32 +0100

    Mon Dec 19 03:42:32 PM CET 2022

--- 19/main.cpp
+++ 19/main.cpp
@@ -56,7 +56,7 @@ LL calc(unsigned char cr1o, unsigned char cr2o, unsigned char cr3o, unsigned cha
         if(time >= 119 && rob3 == 0) continue;
         if(time >= 125 && rob4 == 0) continue;
         if(rob1 > 19 || rob2 > 19 || rob3 > 19) continue;
-        if(count < time){P((int)time, q.size(), (int)rob1, (int)rob2, (int)rob3, (int)rob4, max, maxB);count = time;max = 0;maxB = 0;}
+        // if(count < time){P((int)time, q.size(), (int)rob1, (int)rob2, (int)rob3, (int)rob4, max, maxB);count = time;max = 0;maxB = 0;}
         unsigned char Nres1 = rob1+res1;
         unsigned char Nres2 = rob2+res2;
         unsigned char Nres3 = rob3+res3;
