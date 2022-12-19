924c6d832e32943bc07be334c28c0d5311b71f3a
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-19 06:17:52 +0100

    Mon Dec 19 06:17:52 AM CET 2022

--- 19/main.cpp
+++ 19/main.cpp
@@ -11,12 +11,29 @@ using INT = __int128;
 using VECC = std::vector<char>;
 
 
+LL calc(LL oreCost , LL clayCost , LL obsOreCost, LL obsClayCost, LL geoOreCost, LL goeObsCost)
+{
+    FOR(i, 24)
+    ;
+}
+
+
 int main(int argc, char** argv)
 {
     LL score = 0;
 
+    LL num = 0;
     for(auto& i : in)
     {
+        auto sent = splitStr(i, ' ');
+        LL oreCost     = stoi(sent[ 6]);
+        LL clayCost    = stoi(sent[12]);
+        LL obsOreCost  = stoi(sent[18]);
+        LL obsClayCost = stoi(sent[21]);
+        LL geoOreCost  = stoi(sent[27]);
+        LL goeObsCost  = stoi(sent[30]);
+        P(oreCost, clayCost, obsOreCost, obsClayCost, geoOreCost, goeObsCost);
+        // score += calc(oreCost, clayCost, obsOreCost, obsClayCost, geoOreCost, goeObsCost);
     }
 
     P_RR("Part1: %lld\n", score);
