d6f625bf632ed84e064bc18e4aa3fcfe2c3185f8
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-20 07:15:38 +0100

    Tue Dec 20 07:15:38 AM CET 2022

--- 20/main.cpp
+++ 20/main.cpp
@@ -1,4 +1,4 @@
-// #define TEST
+#define TEST
 
 #ifdef TEST
 #include "in_test.hpp"
@@ -19,50 +19,43 @@ bool D = false;
 int main(int argc, char** argv)
 {
     LL score = 0;
-
-    std::vector<LL> list;
-    std::transform(BE(in), std::back_inserter(list), [](auto& a){return stoi(a);});
-    std::vector<LL> moved(list.size(), -1);
-
-    LL size = list.size()-1;
-    FOR(i, list.size())
     {
-        if(D){P_VEC(list)};
-        // P_VEC(moved);
-        if(moved[i] < 0 && list[i] != 0)
+        std::vector<LL> list;
+        std::transform(BE(in), std::back_inserter(list), [](auto& a){return stoi(a);});
+        std::vector<LL> moved(list.size(), -1);
+
+        LL size = list.size()-1;
+        FOR(i, list.size())
         {
-            LL num = list[i];
-            LL pos = (1000*size + i + num)%size;
-            if(pos == 0){pos = size;}
-            if(D){P(i, num, pos)};
-            list.erase(list.begin() + i);
-            moved.erase(moved.begin() + i);
-            if(pos > i)
+            if(moved[i] < 0 && list[i] != 0)
             {
-                list.insert(list.begin()+pos, num);
-                moved.insert(moved.begin()+pos, i);
-                i--;
-            }
-            else// if(pos < i)
-            {
-                list.insert(list.begin()+pos, num);
-                moved.insert(moved.begin()+pos, i);
+                LL num = list[i];
+                LL pos = (1000*size + i + num)%size;
+                if(pos == 0){pos = size;}
+                list.erase(list.begin() + i);
+                moved.erase(moved.begin() + i);
+                if(pos > i)
+                {
+                    list.insert(list.begin()+pos, num);
+                    moved.insert(moved.begin()+pos, i);
+                    i--;
+                }
+                else// if(pos < i)
+                {
+                    list.insert(list.begin()+pos, num);
+                    moved.insert(moved.begin()+pos, i);
+                }
             }
         }
-    }
-    if(D){P_VEC(list)};
-    size = list.size();
-    LL beg = std::distance(list.begin(), std::find(BE(list), 0));
-    P((beg+1000)%size);
-    P((beg+2000)%size);
-    P((beg+3000)%size);
+        size = list.size();
+        LL beg = std::distance(list.begin(), std::find(BE(list), 0));
 
-    auto n1k = list[(beg+1000)%size];
-    auto n2k = list[(beg+2000)%size];
-    auto n3k = list[(beg+3000)%size];
-    P(n1k, n2k, n3k);
-    score = n1k+n2k+n3k;
-    P_RR("Part1: %lld\n", score);
+        auto n1k = list[(beg+1000)%size];
+        auto n2k = list[(beg+2000)%size];
+        auto n3k = list[(beg+3000)%size];
+        score = n1k+n2k+n3k;
+        P_RR("Part1: %lld\n", score);
+    }
     //========================================================
     score = 0;
 
