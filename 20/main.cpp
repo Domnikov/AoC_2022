4797520b6ccd97c5c7662b847d145bfb32d550ce
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-20 13:46:59 +0100

    Tue Dec 20 01:46:59 PM CET 2022

--- 20/main.cpp
+++ 20/main.cpp
@@ -75,8 +75,8 @@ int main(int argc, char** argv)
         std::transform(BE(list), std::back_inserter(small), [size = list.size()-1](auto& a){
                 LL res = a;
                 P(res);
-                if(res < 0) {res -= (res/size)*size;}
-                while(res < 0){res += size;P(res);}
+                // if(res < 0) {res -= (res/size)*size;}
+                while(res < 0){res += size*100;P(res);}
                 return res%size;
                 // return(1000*size + res%size)%size;
                 });
