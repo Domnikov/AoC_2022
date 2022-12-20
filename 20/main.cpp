6676319fb6c415bbaf7df4317b692ed779a03638
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-20 07:50:36 +0100

    Tue Dec 20 07:50:36 AM CET 2022

--- 20/main.cpp
+++ 20/main.cpp
@@ -69,18 +69,18 @@ int main(int argc, char** argv)
         std::transform(BE(list), std::back_inserter(small), [size = list.size()-1](auto& a){return a%size;});
 
         ULL size = list.size()-1;
-        FOR(n, 10)
+        // FOR(n, 10)
         FOR(ii, list.size())
         {
             LL i = std::distance(order.begin(), std::find(BE(order), ii));
             if(/*moved[i] < 0 &&*/ list[i] != 0)
             {
-                // if(D)P_VEC(list);
+                if(D)P_VEC(list);
                 LL num = list[i];
                 LL numSmall = small[i];
                 LL pos = (1000*size + i + numSmall)%size;
                 if(pos == 0){pos = size;}
-                // if(D)P(i, num, numSmall, pos);
+                if(D)P(i, num, numSmall, pos);
                 list.erase(list.begin() + i);
                 moved.erase(moved.begin() + i);
                 order.erase(order.begin() + i);
