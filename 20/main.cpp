f397a02c67348e9e91a323e38e9c1bbf22fae692
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-20 07:16:43 +0100

    Tue Dec 20 07:16:43 AM CET 2022

--- 20/main.cpp
+++ 20/main.cpp
@@ -58,6 +58,44 @@ int main(int argc, char** argv)
     }
     //========================================================
     score = 0;
+    {
+        std::vector<LL> list;
+        std::transform(BE(in), std::back_inserter(list), [](auto& a){return 811589153*stoi(a);});
+        std::vector<LL> moved(list.size(), -1);
+
+        LL size = list.size()-1;
+        FOR(i, list.size())
+        {
+            if(moved[i] < 0 && list[i] != 0)
+            {
+                if(D)P_VEC(list);
+                LL num = list[i];
+                LL pos = (1000*size + i + num)%size;
+                if(pos == 0){pos = size;}
+                list.erase(list.begin() + i);
+                moved.erase(moved.begin() + i);
+                if(pos > i)
+                {
+                    list.insert(list.begin()+pos, num);
+                    moved.insert(moved.begin()+pos, i);
+                    i--;
+                }
+                else// if(pos < i)
+                {
+                    list.insert(list.begin()+pos, num);
+                    moved.insert(moved.begin()+pos, i);
+                }
+            }
+        }
+        size = list.size();
+        LL beg = std::distance(list.begin(), std::find(BE(list), 0));
+
+        auto n1k = list[(beg+1000)%size];
+        auto n2k = list[(beg+2000)%size];
+        auto n3k = list[(beg+3000)%size];
+        score = n1k+n2k+n3k;
+        P_RR("Part1: %lld\n", score);
+    }
 
     P_RR("Part2: %lld\n", score);
     return 0;
