cd448d28c907571ff1da13be04860d1febe6750c
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-21 11:55:41 +0100

    Wed Dec 21 11:55:41 AM CET 2022

--- 21/main.cpp
+++ 21/main.cpp
@@ -1,9 +1,11 @@
-// #define TEST
+#define TEST
 
 #ifdef TEST
 #include "in_test.hpp"
+    S rm1 = "pppw", rm2 = "sjmn";
 #else
 #include "in.hpp"
+    S rm1 = "fglq", rm2 = "fzbp";
 #endif
 
 #include<queue>
@@ -63,6 +65,54 @@ int main(int argc, char** argv)
     P_RR("Part1: %lld\n", score);
     //========================================================
     score = 0;
+
+    FOR(i, 999999999)
+    {
+        monkeys.clear();
+        monkeys["humn"] = i;
+        while(!monkeys.count(rm1) && !monkeys.count(rm2))
+        {
+            for(auto s : in)
+            {
+                auto vec = splitStr(s, ':');
+                S monkey = vec[0];
+
+                if(!monkeys.count(monkey))
+                {
+                    try
+                    {
+                        LL num = stoll(vec[1]);
+                        monkeys[monkey] = num;
+                    }
+                    catch(const std::invalid_argument& e)
+                    {
+                        auto subVec = splitStr(vec[1], ' ');
+
+                        S m1 = subVec[0];
+                        S op = subVec[1];
+                        S m2 = subVec[2];
+
+                        if(monkeys.count(m1) && monkeys.count(m2))
+                        {
+                            switch(op[0])
+                            {
+                                case '+' : monkeys[monkey] = monkeys[m1] + monkeys[m2]; break;
+                                case '-' : monkeys[monkey] = monkeys[m1] - monkeys[m2]; break;
+                                case '*' : monkeys[monkey] = monkeys[m1] * monkeys[m2]; break;
+                                case '/' : monkeys[monkey] = monkeys[m1] / monkeys[m2]; break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(monkeys.at(rm1) ==  monkeys.at(rm2))
+        {
+            score = i;
+            break;
+        }
+    }
 
     P_RR("Part2: %lld\n", score);
     return 0;
