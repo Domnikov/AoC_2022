e5385770c9f779a112065162ccac2636952ec3ed
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-21 11:40:47 +0100

    Wed Dec 21 11:40:47 AM CET 2022

--- 21/main.cpp
+++ 21/main.cpp
@@ -19,10 +19,46 @@ int main(int argc, char** argv)
 {
     LL score = 0;
 
-    for(auto s : in)
+    std::map<S, LL> monkeys;
+
+    while(!monkeys.count("root"))
     {
-        auto vec = splitStr(s, ' ');
+        for(auto s : in)
+        {
+            auto vec = splitStr(s, ':');
+            S monkey = vec[0];
+
+            if(!monkeys.count(monkey))
+            {
+                try
+                {
+                    LL num = stoll(vec[1]);
+                    monkeys[monkey] = num;
+                }
+                catch(const std::invalid_argument& e)
+                {
+                    auto subVec = splitStr(vec[1], ' ');
+
+                    S m1 = subVec[0];
+                    S op = subVec[1];
+                    S m2 = subVec[2];
+
+                    if(monkeys.count(m1) && monkeys.count(m2))
+                    {
+                        switch(op[0])
+                        {
+                            case '+' : monkeys[monkey] = monkeys[m1] + monkeys[m2]; break;
+                            case '-' : monkeys[monkey] = monkeys[m1] - monkeys[m2]; break;
+                            case '*' : monkeys[monkey] = monkeys[m1] * monkeys[m2]; break;
+                            case '/' : monkeys[monkey] = monkeys[m1] / monkeys[m2]; break;
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    score = monkeys["root"];
 
     P_RR("Part1: %lld\n", score);
     //========================================================
