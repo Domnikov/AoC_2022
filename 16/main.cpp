a1415c98a8ff9f77b85c18d294f9a858460426e1
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-22 19:48:18 +0100

    Thu Dec 22 07:48:18 PM CET 2022

--- 16/main.cpp
+++ 16/main.cpp
@@ -92,6 +92,28 @@ std::pair<LL, LL> countScore(VECI path)
     return {score, time};
 }
 
+
+std::pair<LL, LL> countScore2(VECI path)
+{
+    LL score{};
+    LL time = maxTime;
+    for(LL i = 2; i < path.size(); i+=2)
+    {
+        time -= graph[path[i-1]][path[i]];
+        LL flow = flows[path[i]];
+        if(time > 0) {score += time * flow;}
+        // P(heads[path[i]], time, flow, time*flow, score);
+    }
+    for(LL i = 3; i < path.size(); i+=2)
+    {
+        time -= graph[path[i-1]][path[i]];
+        LL flow = flows[path[i]];
+        if(time > 0) {score += time * flow;}
+        // P(heads[path[i]], time, flow, time*flow, score);
+    }
+    return {score, time};
+}
+
 VECI calc(VECI path, LL time)
 {
     // P_VEC(path);
@@ -168,6 +190,82 @@ VECI calc(VECI path, LL time)
     return res;
 }
 
+
+VECI calc2(VECI path, LL time)
+{
+    // P_VEC(path);
+    VECI res;
+    LL N = heads.size();
+    LL INF = 99999999;
+    std::vector<VECII> V{VECII{}};
+    VECI init{0,9,4,14,6,7,11,3,2,1,12,8,13,15,5,10};
+    // FOR(i, N){init.push_back(i);}
+    FOR(i, N){V.push_back(VECII{init});}
+
+    for(LL k = 0; k < N-1; k++)
+    {
+        VECII newV;
+        // FOR(i, N)
+        {
+            newV.push_back(V.back()[0]);
+            for(LL j = 1; j < N;++j)
+            {
+                P(k, heads[j],V.back().size());
+                LL maxScore = 0;
+                VECI maxVector;
+                for(LL i = 0; i < V.back().size(); ++i)
+                {
+                    // FOR(ii, V[i].size())
+                    // LL ii = V[i].size() - 1;
+                    {
+                        if(i == j) continue;
+                        auto copy = V.back()[i];
+                        if(D){P_PER(copy);P(heads[i], i, j);}
+                        auto it = std::find(BE(copy), j);
+                        if(it == copy.end()){P_LINE;exit(1);}
+                        copy.erase(it);
+                        LL shift = copy.size() - k;
+                        if(shift < 0 || shift > copy.size()){P(shift, k, copy.size());P_LINE;exit(1);}
+                        it = copy.begin() + shift;
+                        copy.insert(it, j);
+                        auto score = countScore2(copy);
+                        if(D){P_PER(copy);P(score.first);}
+                        if(/*score.second > 0 &&*/ score.first > maxScore)
+                        {
+                            maxScore = score.first;
+                            maxVector = copy;
+                        }
+                    }
+                }
+                newV.push_back(maxVector);
+            }
+        }
+        V.push_back(newV);
+        if(D){for(auto& v : V){P_VEC(V);}}
+    }
+
+    LL resScore = 0;
+    LL counter{};
+    for(auto& k : V)
+    {
+        for(auto j: k)
+        {
+            auto jScore = countScore2(j);
+
+            P_PER(j);
+            P(jScore.first);
+            if(resScore < jScore.first)
+            {
+                resScore = jScore.first;
+                res = j;
+            }
+        }
+
+        P(counter++, resScore);
+    }
+
+    return res;
+}
 // LL brute()
 
 int main(int argc, char** argv)
@@ -216,6 +314,20 @@ int main(int argc, char** argv)
     //========================================================
     score = 0;
     D = false;
+
+    path = calc2({0}, 0);
+
+    sc = countScore2(path);
+    P_VEC(path);
+    while(sc.second < 0)
+    {
+        path.pop_back();
+        sc = countScore2(path);
+        P_VEC(path);
+        P(sc.first, sc.second);
+    }
+
+    score = countScore2(path).first;
 
     P_RR("Part2: %lld\n", score);
     return 0;
