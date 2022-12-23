43ccc1e535cb473c86b271d4b335bca06902c13f
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-24 00:33:07 +0100

    Sat Dec 24 12:33:07 AM CET 2022

--- 16/main.cpp
+++ 16/main.cpp
@@ -95,6 +95,7 @@ std::pair<LL, LL> countScore(VECI path)
 
 std::pair<LL, LL> countScore2(VECI path)
 {
+    P_VEC(path);
     LL score{};
     LL time = maxTime;
     for(LL i = 2; i < path.size(); i+=2)
@@ -102,7 +103,7 @@ std::pair<LL, LL> countScore2(VECI path)
         time -= graph[path[i-1]][path[i]];
         LL flow = flows[path[i]];
         if(time > 0) {score += time * flow;}
-        // P(heads[path[i]], time, flow, time*flow, score);
+        P(heads[path[i]], time, flow, time*flow, score);
     }
     time = maxTime;
     for(LL i = 3; i < path.size(); i+=2)
@@ -110,7 +111,7 @@ std::pair<LL, LL> countScore2(VECI path)
         time -= graph[path[i-1]][path[i]];
         LL flow = flows[path[i]];
         if(time > 0) {score += time * flow;}
-        // P(heads[path[i]], time, flow, time*flow, score);
+        P(heads[path[i]], time, flow, time*flow, score);
     }
     return {score, time};
 }
