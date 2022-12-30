f106c7153f34273c09b8b447cb2532819a1652c0
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-31 00:30:14 +0100

    Sat Dec 31 12:30:14 AM CET 2022

--- 16/main.cpp
+++ 16/main.cpp
@@ -308,7 +308,7 @@ int main(int argc, char** argv)
 
     VECI init;
     init.push_back(0);
-    FOR(i, graph.size()-1){init.push_back(graph.size() - 1 - i+1);}
+    FOR(i, graph.size()-1){init.push_back(graph.size() - 1 - i);}
     P_VEC(init);
     VECI newPath = init, path;
     do
