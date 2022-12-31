67c95c99a2a261b9acfd8b04ea498767b2a93cae
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-31 01:10:25 +0100

    Sat Dec 31 01:10:25 AM CET 2022

--- 22/main.cpp
+++ 22/main.cpp
@@ -14,6 +14,10 @@ using INT = __int128;
 using VECC = std::vector<char>;
 bool D = false;
 
+enum DIR
+{
+    up,down,left,right
+};
 
 int main(int argc, char** argv)
 {
@@ -21,13 +25,21 @@ int main(int argc, char** argv)
 
     std::map<S, INT> monkeys;
 
+
+    auto cmds = *(in.end()-1);
+
+    LL x = in[0].find('.');
+    LL y = 0;
+    DIR dir = DIR::right;
+
+    in[y][x] = 'X';
+
     FOR(i, in.size()-2)
     {
         auto s = in[i];
         P(s);
     }
 
-    P(*(in.end()-1));
 
 
     P_RR("Part1: %lld\n", score);
