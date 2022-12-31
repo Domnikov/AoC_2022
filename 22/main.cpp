755443396a525943a4988ee46651a557b4b5fbba
Domnikov Ivan <ivan.mail_list@yahoo.com>
2022-12-31 20:25:21 +0100

    Sat Dec 31 08:25:21 PM CET 2022

--- 22/main.cpp
+++ 22/main.cpp
@@ -74,9 +74,9 @@ bool changeDir()
 
 void flipUp   ()
 {
-    auto posDot = in.size() + std::distance(in.rend(), std::find_if(in.rend()+2, in.rbegin(), [](const auto& s){return s[x]== '.';}));
-    auto posDsh = in.size() + std::distance(in.rend(), std::find_if(in.rend()+2, in.rbegin(), [](const auto& s){return s[x]== '#';}));
-    auto posOoo = in.size() + std::distance(in.rend(), std::find_if(in.rend()+2, in.rbegin(), [](const auto& s){return s[x]== 'o';}));
+    auto posDot = in.size() + std::distance(in.rbegin(), std::find_if(in.rend()+2, in.rbegin(), [](const auto& s){return s[x]== '.';}));
+    auto posDsh = in.size() + std::distance(in.rbegin(), std::find_if(in.rend()+2, in.rbegin(), [](const auto& s){return s[x]== '#';}));
+    auto posOoo = in.size() + std::distance(in.rbegin(), std::find_if(in.rend()+2, in.rbegin(), [](const auto& s){return s[x]== 'o';}));
 
     P(posDot, posDsh, posOoo);
 
