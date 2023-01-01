9f8e44cddd57bf110e917b5e541efd24e2250fd3
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-01-01 20:51:28 +0100

    Sun Jan  1 08:51:28 PM CET 2023

---
+++ 23/main.cpp
@@ -0,0 +1,24 @@
+#define TEST
+
+#ifdef TEST
+#include "in_test.hpp"
+#else
+#include "in.hpp"
+#endif
+
+#include<queue>
+#include <list>
+
+
+int main(int argc, char** argv)
+{
+    LL score = 0;
+
+
+    P_RR("Part1: %lld\n", score);
+    //========================================================
+    score = 0;
+
+    P_RR("Part2: %lld\n", score);
+    return 0;
+}
