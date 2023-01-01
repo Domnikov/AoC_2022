9c5409672aa9a830a0e71f370798680dd19d5ac8
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-01-01 21:25:12 +0100

    Sun Jan  1 09:25:12 PM CET 2023

--- 23/main.cpp
+++ 23/main.cpp
@@ -9,11 +9,22 @@
 #include<queue>
 #include <list>
 
+auto in = getInput();
+
+void checkLand()
+{
+    if(in[          0].find('#') != std::string::npos)                            {in.insert(in.begin(), S(in[          0].size(), '.'));}
+    if(in[in.size()-1].find('#') != std::string::npos)                            {in.insert(in.end  (), S(in[in.size()-1].size(), '.'));}
+    if(std::any_of(in.begin(), in.end(), [](const auto& s){return s[         0] == '#';})) {std::for_each(in.begin(), in.end(), [](auto s){return s.insert(s.begin(), '.');});}
+    if(std::any_of(in.begin(), in.end(), [](const auto& s){return s[s.size()-1] == '#';})) {std::for_each(in.begin(), in.end(), [](auto s){return s.insert(s.end  (), '.');});}
+}
+
 
 int main(int argc, char** argv)
 {
     LL score = 0;
 
+    P_VEC(in);
 
     P_RR("Part1: %lld\n", score);
     //========================================================
