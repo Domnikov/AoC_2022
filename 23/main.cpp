d6f7c9e2c55458fdb66fd4f1b37aa34b4dd832b9
Domnikov Ivan <ivan.mail_list@yahoo.com>
2023-01-01 21:39:26 +0100

    Sun Jan  1 09:39:26 PM CET 2023

--- 23/main.cpp
+++ 23/main.cpp
@@ -22,8 +22,8 @@ void checkLand()
 
 LL checkScore()
 {
-    auto countDashs = [](LL res, char c){return res + c=='#';};
-    LL res = std::accumulate(in.begin()+1, in.end()-1, res, [countDashs](const auto& res, const auto& s){return res + std::accumulate(s.begin()+1, s.end()-1, 0, countDashs );});
+    auto countDashs = [](LL res, char c){return res + c=='#' ? 1 : 0;};
+    LL res = std::accumulate(in.begin()+1, in.end()-1, 0, [countDashs](const auto& res, const auto& s){return res + std::accumulate(s.begin()+1, s.end()-1, 0, countDashs );});
     return res;
 }
 
